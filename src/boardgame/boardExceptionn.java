12ef13ecab4085dfbb1f4079cc9fa45dadd8e121
Vinicius Gonçalves <viniciiussilva97@outlook.com>
2023-05-29 16:13:21 -0300

    Tratando Exceções e programa defensivo

---
+++ src/boardgame/boardExceptionn.java
@@ -0,0 +1,12 @@
+package boardgame;
+
+public class boardExceptionn extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public boardExceptionn(String msg) {
+		super(msg);
+	}
+	
+
+}
