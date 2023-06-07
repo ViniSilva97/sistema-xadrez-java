0fca05804fed9f8c845b4be0ab8407f8ca550615
Vinicius Gonçalves <viniciiussilva97@outlook.com>
2023-06-07 10:53:58 -0300

    Manipulação das peças capturadas

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import boardgame.ChessPiece;
 import chess.ChessMatch;
@@ -48,9 +51,10 @@ public class UI {
 		}
 	}
 	
-	public static void printMatch(ChessMatch chessMatch) {
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getpieces());
 		System.out.println();
+		printCapturedPieces(captured);
 		System.out.println("Vez: " + chessMatch.getTurn());
 		System.out.println("Esperando o player " + chessMatch.getCurrentPlayer());
 	}
@@ -95,6 +99,25 @@ public class UI {
             }
         }
         System.out.print(" ");
+	}
+	
+	private static void printCapturedPieces(List<ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Peca Capurada");
+		System.out.print("Verdes: ");
+		System.out.print(ANSI_GREEN);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.println(ANSI_RESET);
+		
+		
+		System.out.print("Amarelas: ");
+		System.out.print(ANSI_YELLOW);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.println(ANSI_RESET);
+		
+		
+
 	}
 
 }
